c52dcb33db3ecca56ebcd36f53873434a02677d3
Glitch (hello-express) <none>
2019-01-02 18:44:41 +0000

    🐁🎠 Checkpoint ./commands/profile.js:1904606/32:1954975/45

--- commands/profile.js
+++ commands/profile.js
@@ -4,7 +4,6 @@ const eco = require("discord-economy")
 module.exports.run = async (bot, message, args) => {
   let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member
   let userEco = await eco.FetchBalance(user.user.id).then()
-  console.log(userEco)
   let roles = user.roles;
         let role_name = roles.map(role => {
           if(role.name === "@everyone") return
@@ -14,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
         let joined = user.joinedAt
         var profile = new Discord.RichEmbed()
             .addField("Name", user)
-            .addField("Joined at", joined)
+            .addField("Joined at", joined) //what happened to the thumbnail
             .addField("Coins", userEco.balance)//no that is array  //wait what do add here then
             .addField("Roles", role_name)
             .setColor(0xFF0000)
