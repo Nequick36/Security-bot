28cc6f390bef3b1150f9d6239cefc96aa59b785e
Glitch (hello-express) <none>
2019-01-02 13:55:56 +0000

    💡🐷 Checkpoint ./commands/dice_roll.js:1954975/302

---
+++ commands/dice_roll.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+  var diceRoll = Math.floor(Math.random() * 6) + 1;
+    message.reply("Your dice landed on " + diceRoll);
+}
+
+module.exports.help = {
+  name: "roll",
+  description: "Rolls a six sided dice.",
+  group:"SIMPLE"
+}
