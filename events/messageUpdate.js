2fb9daf28b86905b038dee8d0516e1ec8efc7785
Glitch (hello-express) <none>
2019-01-02 22:32:13 +0000

    🍌🍿 Checkpoint ./events/messageUpdate.js:1904606/11

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const discord = require('discord.js')
 exports.run = async (bot, oldMessage, newMessage) => {
-  let logChannel = message.guild.channels.find(c => c.name === 'chat-logs')
+  let logChannel = oldMessage.guild.channels.find(c => c.name === 'chat-logs')
   logChannel.send(`Message edited: 
 Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${oldMessage.content}
 New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${newMessage.content}`)
