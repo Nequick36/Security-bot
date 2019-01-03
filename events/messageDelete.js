a25362fcc24f1ac04ee2b60207ef73d4534c0ac3
Glitch (gamehub--2) <none>
2019-01-03 22:31:59 +0000

    👭🕋 Checkpoint ./events/userUpdate.js:1904606/56 ./events/messageUpdate.js:1904606/287 ./events/messageDelete.js:1904606/121

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const discord = require('discord.js')
+const replaceall = require('replaceall')
 exports.run = async (bot, message) => {
   let logChannel = message.guild.channels.find(c => c.name === 'chat-logs')
-  logChannel.send(`Message deleted: [${message.channel.name}] ${message.author.username}: ${message.content.replace('<@', '<')}`)
+  logChannel.send(`Message deleted: [${message.channel.name}] ${message.author.username}: ${replaceall('<@', '<', message.content)}`)
   console.log(`Message deleted: [${message.channel.name}] ${message.author.username}: ${message.content}`);
 }
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const discord = require('discord.js')
+const replaceall = require('replaceall')
 exports.run = async (bot, oldMessage, newMessage) => {
   let logChannel = oldMessage.guild.channels.find(c => c.name === 'chat-logs')
     if(oldMessage.author.bot) {}
   else {
   logChannel.send(`Message edited: 
-Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${oldMessage.content.replace('<@', '<')}
-New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${newMessage.content.replace('<@', '<')}`)
+Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${replaceall('<@', '<', oldMessage.content)}
+New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${replaceall('<@', '<', newMessage.content)}`)
   console.log(`Message edited: 
 Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${oldMessage.content}
 New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${newMessage.content}`);
