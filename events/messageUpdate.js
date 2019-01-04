23c4b7487a285d8494f8cbcebf705ab392f15d68
Glitch (gamehub--2) <none>
2019-01-04 21:07:57 +0000

    👜🎳 Checkpoint ./events/messageUpdate.js:1904606/110

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -5,6 +5,7 @@ exports.run = async (bot, oldMessage, newMessage) => {
   let logChannel = oldMessage.guild.channels.find(c => c.name === '🔸》chat-logs')
     if(oldMessage.author.bot) {}
   else {
+    if(oldMessage.content.includes('https://') && newMessage.content.includes('https://')) return
 logChannel.send(`Message edited: 
 Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${replaceall('<@', '<', oldMessage.content)}
 New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${replaceall('<@', '<', newMessage.content)}`)
