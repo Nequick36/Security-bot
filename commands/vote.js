9d2148198c54e9a1d9b73a14c218d63cbac161b0
Glitch (gamehub--2) <none>
2019-01-04 19:40:59 +0000

    🏋🍎 Checkpoint ./commands/vote.js:1904606/233

--- commands/vote.js
+++ commands/vote.js
@@ -1,21 +1,22 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  let emoji = ['🇦', '🇧', '🇨', '', '']//:regional_indicator_a: :regional_indicator_b: :regional_indicator_c: 
+  let emoji = ['', '🇦', '🇧', '🇨', '', '']//:regional_indicator_a: :regional_indicator_b: :regional_indicator_c: 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't make poll!")
   let options = args.join(" ").split(" | ")
-  if(options.length >= 5) return message.reply("Too many options!")
+  if(options.length >= 6) return message.reply("Too many options!")
   let optionsRAW = ''
   let embed = new Discord.RichEmbed()
-  .setTitle("Poll")
-  for(let i = 0; i<options.length; i++) {
+  .setTitle(options[0])
+  .setColor(0xff0000)
+  for(let i = 1; i<options.length; i++) {
     optionsRAW = `${optionsRAW}${emoji[i]} ${options[i]}\n`
   }
-
-  let msg = message.channel.send(optionsRAW)
+  embed.setDescription(optionsRAW)
+  let msg = await message.channel.send(embed)
   
-  for(let l = 0; l<options.length; l++) {
-    msg.react(emoji[l])
+  for(let l = 1; l<options.length; l++) {
+    await msg.react(emoji[l])
   }
 }
 
