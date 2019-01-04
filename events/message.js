3d59e779a4e8f85210c0eebb3bf0c0a4c2cc11f9
Glitch (gamehub--2) <none>
2019-01-04 21:40:58 +0000

    👱🌉 Checkpoint ./events/message.js:1904606/703 ./events/msgs.json:1904606/2

--- events/message.js
+++ events/message.js
@@ -3,4 +3,18 @@ const discord = require('discord.js')
 exports.run = async (bot, message) => {
   let logChannel = message.guild.channels.find(c => c.name === '🔸》moderation-chat-logs')
   if(message.content.startsWith('!')) logChannel.send({embed:{description: `${message.author.tag} used command ${message.content} in channel ${message.channel.name}.`, color: 0xff0000}})
+  
+  
+  
+  //message count
+  let msgs = JSON.parse(fs.readFileSync('events/msgs.json', 'utf8'));
+  let author = message.author
+  if(!msgs[author.id]) msgs[author.id] = {}
+  if(!msgs[author.id].username) msgs[author.id].username = author.username
+  if(msgs[author.id].username !== author.username) msgs[author.id].username = author.username
+  if(!msgs[author.id].count) msgs[author.id].count = 0
+  msgs[author.id].count++
+  fs.writeFile('events/msgs.json', JSON.stringify(msgs, null, 4), (err) => {
+     if(err) console.error(err)
+  })
 }
