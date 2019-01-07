039df2b3480451ef653bc955726001742c59ca99
Glitch (gamehub--2) <none>
2019-01-07 16:54:38 +0000

    🔦🚠 Checkpoint ./events/messageDeleteBulk.js:1954975/213

--- events/messageDeleteBulk.js
+++ events/messageDeleteBulk.js
@@ -1,1 +1,7 @@
 const Discord = require("discord.js")
+const replaceall = require('replaceall')
+
+exports.run = async (bot, messages) => {
+  let logChannel = messages.guild.channels.find(`name`, "🔸》chat-logs")
+  logChannel.send(``)
+}
