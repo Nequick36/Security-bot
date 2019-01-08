2da23f1242498246d02dc328dfc0307ab92b3daf
Glitch (gamehub--2) <none>
2019-01-08 21:22:14 +0000

    🍐⛄️ Checkpoint ./events/guildMemberUpdate.js:1904606/257

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const discord = require('discord.js')
 exports.run = async (bot, oldMember, newMember) => {
   let logChannel = oldMember.guild.channels.find(c => c.name === '🔸》chat-logs')
-  logChannel.send(`\`\`\`${newMember}\`\`\``)
-  let embed = new discord.RichEmbed()
-  .setTitle(oldMember.user.tag)
-  .addField("Old nickname", oldMember.nickname)
-  .addField("New nickname", newMember.nickname)
-  .setTHumbnail(oldMember.avatarURL)
+  let nickEmbed = new discord.RichEmbed()
+  nickEmbed.setTitle(newMember.user.tag)
+  nickEmbed.setColor('BLUE')
+  nickEmbed.addField("Old nickname", oldMember.nickname)
+  nickEmbed.addField("New nickname", newMember.nickname)
+  nickEmbed.setThumbnail(oldMember.avatarURL)
   if(oldMember.nickname !== newMember.nickname) {
-    logChannel.send(embed)
+    logChannel.send(nickEmbed)
   }
 }
