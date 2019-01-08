5c0d8ad4cde4e2929cb23caff19d2e0e70cd6949
Glitch (gamehub--2) <none>
2019-01-08 16:33:37 +0000

    🍎🌱 Checkpoint ./events/messageUpdate.js:1904606/26:1954975/743 ./watch.json:1954975/2

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -5,16 +5,13 @@ exports.run = async (bot, oldMessage, newMessage) => {
   let logChannel = oldMessage.guild.channels.find(c => c.name === '🔸》chat-logs')
   let embed = new discord.RichEmbed()
   .setTitle(`${oldMessage.author} has edited a message`)
-  .setThumbnail(oldMessage.user.avatarURL)
-  .addField("Before", `${}`)
+  .setThumbnail(oldMessage.author.displayAvatarURL)
+  .addField("Before", oldMessage.content)
+  .addField("After", `${newMessage.content}`)
+  .addField("Message edited in", `${oldMessage.channel}`)
     if(oldMessage.author.bot) {}
   else {
     if(oldMessage.content.includes('https://') && newMessage.content.includes('https://')) return
-logChannel.send(`Message edited: 
-Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${replaceall('<@', '<', oldMessage.content)}
-New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${replaceall('<@', '<', newMessage.content)}`)
-  console.log(`Message edited: 
-Old Message: [${oldMessage.channel.name}] ${oldMessage.author.username}: ${oldMessage.content}
-New Message: [${newMessage.channel.name}] ${newMessage.author.username}: ${newMessage.content}`);
+logChannel.send(embed);
   }
 }
