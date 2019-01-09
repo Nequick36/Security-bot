b152b93fbd05d162a7afb01ca744538f796e5879
Glitch (gamehub--2) <none>
2019-01-09 17:36:45 +0000

    🗻🗃 Checkpoint ./events/messageDeleteBulk.js:1954975/50

--- events/messageDeleteBulk.js
+++ events/messageDeleteBulk.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js")
 
 exports.run = async (bot, messages) => {
- let mChannel = messages.guild.channels.find(c => c.name === '🔸》moderation-chat-logs')
+ let mChannel = bot.guilds.get('358634388461715456').channels.find(c => c.name === '🔸》moderation-chat-logs')
  mChannel.send({embed:{description: `**Bulk delete: ${messages.size} messages **`, color:0xff0000}})
 }
