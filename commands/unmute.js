7be9f3111bde706793374fbc63a58e19d8aee44b
Glitch (gamehub--2) <none>
2019-01-11 22:06:11 +0000

    🛰👁 Checkpoint ./commands/unmute.js:1904606/102:1954975/69 ./watch.json:1954975/2

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let role = mess
+ let role = message.guild.roles.find(r => r.name === 'Muted') 
   let user = message.mentions.members.first()
- if(!message.mentions.members.first().roles.has(role.id)) return message.reply("That user is not muted!")
-   if(!message.author.hasPermission("MANAGE_MESSAGES")) return message.reply("🛑 **ACCESS DENIED! THIS IS A MOD/ADMIN ONLY COMMAND. 🛑**")
+  
+ if(!user.roles.has(role.id)) return message.reply("That user is not muted!")
+ if(!message.author.hasPermission("MANAGE_MESSAGES")) return message.reply("🛑 **ACCESS DENIED! THIS IS A MOD/ADMIN ONLY COMMAND. 🛑**")
   
   user.removeRole(role.id) 
    message.reply(`${user} has been unmuted!`)
