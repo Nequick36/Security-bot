74c0468ab973cb3811e808c2d8fc2cabc2f8c406
Glitch (gamehub--2) <none>
2019-01-11 20:22:07 +0000

    🌥🏞 Checkpoint ./commands/resetwarns.js:1954975/42 ./watch.json:1954975/2

--- commands/resetwarns.js
+++ commands/resetwarns.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send("⛔**ACCESS DENIED! THIS IS A STAFF-ONLY COMMAND.⛔**");
         return;
     }
-    var user = message.mentions.users.first() || message.guild.members.get(args[0])
+    var user = message.mentions.users.first() 
     if (!user) return message.reply('cannot find user!')
     let warning = message.content.split(" ").slice(2).join(" ");
     if(!warns[user.id]) warns[user.id] = {}
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
    if(warns[user.id].reasons) warns[user.id].reason
    warns[user.id].warns = 0
    warns[user.id].reasons = ''
-   message.channel.send({embed:{description:`Reseted ${user.user.username}'s warnings to 0.`, color:0xff0000}})
+   message.channel.send({embed:{description:`Reseted ${user.username}'s warnings to 0.`, color:0xff0000}})
    fs.writeFile('commands/warns.json', JSON.stringify(warns, null, 4), (err) => {
 if(err) console.error(err)
 })
