02117f7cb26f25eca181c6d224c0f32144b31387
Glitch (gamehub--2) <none>
2019-01-13 15:34:17 +0000

    🍳🙀 Checkpoint ./commands/ping.js:1954975/463 ./index.js:1954975/167

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+  
+}
+
+module.exports.help = {
+  name: "ping",
+  description: "Gets the bot's average ping",
+  group:"SIMPLE"
+}
