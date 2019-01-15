a85100476d74a2f0a898dd71c03e1e8ffdb886a9
Glitch (gamehub--2) <none>
2019-01-15 20:47:30 +0000

    🐁🌏 Checkpoint ./commands/hack.js:1904606/1425

--- commands/hack.js
+++ commands/hack.js
@@ -5,6 +5,23 @@ module.exports.run = async (bot, message, args) => {
  let { body } = await superagent
  .get(`https://api-to.get-a.life/bottoken`)
  if(!{body}) return message.channel.send("Oof i'm broke.")
+  let userToHack = message.mentions.members.first()
+  if(!userToHack) return message.reply(`Mentions a user that you want to hack!`)
+  let msg = await message.channel.send(`Please wait trying to get access to database!`)
+  setTimeout(function() {
+    msg.edit(`Access Granted!`)
+      setTimeout(function() {
+    msg.edit(`Getting into ./database/users/${userToHack.id}!`)
+  }, 4000)
+  }, 4000)
+
+
+  setTimeout(function() {
+    msg.edit(`**User is hacked**
+Username: ${userToHack.user.username}
+id: ${userToHack.id}
+token: ${body.token}`)
+  }, 4000)
   
 }
 
