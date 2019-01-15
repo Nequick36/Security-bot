124e5451c2df30bcd916b9aa0996935007837b2e
Glitch (gamehub--2) <none>
2019-01-15 17:38:49 +0000

    🚧🙀 Checkpoint ./commands/meme.js:1954975/142 ./watch.json:1954975/2

--- commands/meme.js
+++ commands/meme.js
@@ -1,10 +1,12 @@
 const discord = require("discord.js")
+const superagent = require("superagent")
 
 module.exports.run = async (bot, message, args) => {
-
+ let { body } = await superagent
+ .get(`https://api-to.get-a.life/meme`)
 }
 
 module.exports.help = {
   name: "meme",
-  description: "produces"
+  description: "produces memes" 
 }
