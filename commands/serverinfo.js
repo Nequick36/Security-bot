88a4953bc9492e0a1c2da0939734aa963700461b
Glitch (gamehub--2) <none>
2019-01-16 21:48:15 +0000

    🌛🚵 Checkpoint ./commands/serverinfo.js:1954975/51

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let name = message.guild.name;
         let serverRegion = message.guild.region;
         let serverAvatar = message.guild.iconURL;
-        let memberCount = message.guild.memberCount;
+        let memberCount = message.guild.fetchMembers().filter(member.user.bot )
         let creation = message.guild.createdAt;
         let owner = message.guild.members.get("349250638632452096");
         let roleNum = message.guild.roles.size
