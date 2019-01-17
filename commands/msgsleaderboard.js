5f444677e3d1e6beaba5bfa9b7f660b0abc286b9
Glitch (gamehub--2) <none>
2019-01-17 19:15:37 +0000

    👷🍤 Checkpoint ./commands/msgsleaderboard.js:1904606/503

---
+++ commands/msgsleaderboard.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+const fs = require('fs') //what have you done here
+
+module.exports.run = async (bot, message, args) => {
+  let msgs = JSON.parse(fs.readFileSync('events/msgs.json', 'utf8'));
+  message.channel.send({embed:{description:`You have sent ${msgs[message.author.id].count || '0'} messages!`, color:0xff0000}})
+}
+
+module.exports.help = {
+  name: "msgsleaderboard",
+  description: "Shows leaderboard of user.",
+  group:"Simple"
+}
