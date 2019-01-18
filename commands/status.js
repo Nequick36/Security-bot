ae85d49672b24f8bd7ac567733b004e0cb7b1052
Glitch (gamehub--2) <none>
2019-01-18 16:07:05 +0000

    🕹📹 Checkpoint ./commands/status.js:1904606/538:1954975/220 ./commands/unban.js:1954975/4 ./index.js:1954975/19

--- commands/status.js
+++ commands/status.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
+  let devRole = message.guild.roles.find(`name`, "GH Bot Developer")
+  if(!message.member.roles.has(devRole.id)) return message.channel.send("**🛑 THIS IS A DEVELOPER ONLY COMMAND! 🛑**")
     // bot.user.setStatus('available')
     // bot.user.setPresence({
     //     game: {
@@ -8,13 +10,19 @@ module.exports.run = async (bot, message, args) => {
     //         type: "WATCHING",
     //     }
     // })
-  if(!args[0]) {
+  if(!args[1]) {
     try {
       bot.user.setStatus(args[0])
+    } catch(e) {
+      if(e) message.channel.send(e)
     }
-    catch(e) {
-      if(e) message.reply(`${e}`)
-    }
+  } else {
+    bot.user.setPresence({
+        game: {
+            name: args.shift().join(" "),
+            type: args[0],
+        }
+    })
   }
 }
 
