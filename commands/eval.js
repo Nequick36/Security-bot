810ee33de6358f433494c6ed3dc8d72a29b4de34
Glitch (gamehub--2) <none>
2019-01-19 17:48:08 +0000

    💃🚅 Checkpoint ./commands/eval.js:1904606/563

---
+++ commands/eval.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+    let 
+}
+
+module.exports.help = {
+  name: "eval",
+  aliases: [],
+  description: "Evals a code.",
+  perm: "ADMINISTRATOR",
+  role: "GH Bot Developer",
+  group:"Developer"
+}
