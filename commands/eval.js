6d1860926030f30ce2657fa41fac1281f2038eb8
Glitch (gamehub--2) <none>
2019-01-19 18:21:03 +0000

    🌉🚔 Checkpoint ./index.js:1904606/52 ./commands/eval.js:1904606/315 ./handler/functions.js:1904606/117

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== '435406608479158273' && message.author.id !== '366234394479951873') return message.channel.send(`🛑 **ACCESS DENIED! THIS IS A DEVELOPER ONLY COMMAND. 🛑**`)
   let embed = new Discord.RichEmbed()
   let code = args.join(" ")
   if(!code) return message.channel.send('What code do you want to eval?')
@@ -11,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
     .addField('Output', output)
     message.channel.send(embed)
   } catch(e) {
-    if(e) embed.addField('Error', e)
+    embed.addField('Error', `${e || 'No errors'}`)
   }
 }
 
