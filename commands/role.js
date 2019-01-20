24dbe961945a871d0dc9fb8e7d292110fe5fbcf2
Glitch (gamehub--2) <none>
2019-01-20 21:32:09 +0000

    💄🙄 Checkpoint ./commands/role.js:1904606/753

---
+++ commands/role.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+  let user = message.member
+  if(user.nickname) {
+    if(user.nickname.startsWith('[AFK]')) {user.setNickname(user.nickname.replace('[AFK] ', ''))
+                                           message.channel.send(`You are no longer AFK!`)}
+    else {user.setNickname(`[AFK] ${user.nickname}`)
+          message.channel.send(`You are now AFK!`)}
+  } else {user.setNickname(`[AFK] ${user.username}`)
+          message.channel.send(`You are now AFK!`)}
+}
+
+module.exports.help = {
+  name: "role",
+  aliases: [],
+  description: "Command used to control roles.",
+  perm: "",
+  role: "",
+  group:"Simple"
+}
