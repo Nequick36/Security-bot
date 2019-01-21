83dabcf62a9770c04401e2e5cac9c702f50e2d88
Glitch (gamehub--2) <none>
2019-01-21 12:31:06 +0000

    🗻🍐 Checkpoint ./commands/role.js:1904606/31

--- commands/role.js
+++ commands/role.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
   let role = message.guild.roles.find(r => r.name === args.join(" "))
-  if(!role) return message.channel.send(`Can't find role named ${role.name}`)
+  if(!role) return message.channel.send(`Can't find role named ${args.join(" ")}`)
   let members = message.guild.members.filter(m => m.roles.has(role.id))
   message.channel.send(`${members.size} member(s) has role ${role.name}`)
   }   
