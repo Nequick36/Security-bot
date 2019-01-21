d7ba9d13932809ba066b528f689a232a90e4f372
Glitch (gamehub--2) <none>
2019-01-21 17:02:37 +0000

    🌅🍑 Checkpoint ./commands/role.js:1954975/45

--- commands/role.js
+++ commands/role.js
@@ -4,6 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let role = message.guild.roles.find(r => r.name === args.join(" "))
   if(!role) return message.channel.send(`Can't find role named ${args.join(" ")}`)
   let members = message.guild.members.filter(m => m.roles.has(role.id))
+  let rEmbed = new Discord.RichEmbed()
   message.channel.send(`${members.size} member(s) has role ${role.name}`)
   }   
 
