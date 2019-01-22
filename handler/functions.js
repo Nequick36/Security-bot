3cd13a197e5dd9b6663af1cfd603df5dd127b9d7
Glitch (gamehub--2) <none>
2019-01-22 17:53:31 +0000

    🍃🏵 Checkpoint ./handler/functions.js:1904606/3 ./index.js:1954975/377 ./watch.json:1954975/2

--- handler/functions.js
+++ handler/functions.js
@@ -1,6 +1,6 @@
 let Discord = require('discord.js')
 module.exports = {
-  embed = async function(title, color, description, channel) {
+  embed: async function(title, color, description, channel) {
     let CustomEmbed = new Discord.RichEmbed()
     .setTitle(`${title || ''}`)
     .setColor(`${color || 0xff000}`)
