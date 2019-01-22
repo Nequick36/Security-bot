042533b93db24651709785f5f19b06ae130581b1
Glitch (gamehub--2) <none>
2019-01-22 18:25:51 +0000

    📹🐷 Checkpoint ./index.js:1904606/4 ./commands/sql.js:1904606/1374

--- commands/sql.js
+++ commands/sql.js
@@ -1,10 +1,24 @@
 const Discord = require("discord.js")
-const sqlite = require('sqlite3')
+const sqlite = require('sqlite3').verbose()
+var fs = require('fs');
 
 module.exports.run = async (bot, message, args) => {
- message.guild.members.forEach(member => {
-   
- })
+
+  var dbFile = './database.sqlite';
+  var dbExists = fs.existsSync(dbFile);
+
+  if (!dbExists) {
+      fs.openSync(dbFile, 'w');
+  }
+
+  var db = new sqlite.Database(dbFile);
+  
+  message.guild.members.forEach(member => {
+    
+  })
+
+  db.close();
+
 }
 
 module.exports.help = {
