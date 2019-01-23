3bce63ef78d0ac953cf005792d50651ba6a420d1
Glitch (gamehub--2) <none>
2019-01-23 12:03:41 +0000

    📉🌎 Checkpoint ./commands/shorturl.js:1904606/855

--- commands/shorturl.js
+++ commands/shorturl.js
@@ -4,16 +4,32 @@ const isgd = require('isgd')
 
 module.exports.run = async (bot, message, args) => {
   let type = args[0]
-  let link = args.slice(1).join(" ")
+  let link = args.[1].join(" ")
+  let custom = args.slice(2).join(" ")
+  console.log(`
+type ${type}
+link ${link}
+custom ${custom}
+`)
   
   if(type === 'shorten') {
     isgd.shorten(link, function(res) {
     message.channel.send(res); // Returns a shorter version of http://google.com - http://is.gd/OwycZW
 }); 
-  } else if(type === '') {
-  
-  } else if (type === '') {
-    
+  } else if(type === 'custom') {
+  isgd.custom(link, custom, function(res) {
+    message.channel.send(res); // If the custom URL of MyGoogleShortcut is actually available, it should return http://is.gd/MyGoogleShortcut
+});
+  } else if (type === 'lookup') {
+    isgd.lookup(link, function(res) {
+    console.log(res); // Returns a longer version of http://is.gd/OwycZW - http://google.com
+});
+  } else {
+    message.channel.send(`Short URL help:
+!shorturl shorten - just shorts an URL
+!shorturl custom - shorts an URL with custom name
+!shorturl lookup - unshorts an URL
+`)
   }
 }
 
