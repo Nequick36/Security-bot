c05d7684f9dcad7e36894d28461a0499a5c98489
Glitch (gamehub--2) <none>
2019-01-23 12:14:52 +0000

    🚈🍌 Checkpoint ./commands/shorturl.js:1904606/101

--- commands/shorturl.js
+++ commands/shorturl.js
@@ -4,13 +4,8 @@ const isgd = require('isgd')
 
 module.exports.run = async (bot, message, args) => {
   let type = args[0]
-  let link = args.[1].join(" ")
-  let custom = args.slice(2).join(" ")
-  console.log(`
-type ${type}
-link ${link}
-custom ${custom}
-`)
+  let link = args[1]
+  let custom = args[2]
   
   if(type === 'shorten') {
     isgd.shorten(link, function(res) {
