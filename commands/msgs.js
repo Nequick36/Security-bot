0cd2ee742d5b08367099fad16866014703397733
Glitch (gamehub--2) <none>
2019-01-26 21:57:52 +0000

    👄🍮 Checkpoint ./commands/msgs.js:1954975/172 ./watch.json:1954975/4

--- commands/msgs.js
+++ commands/msgs.js
@@ -4,7 +4,8 @@ const fs = require('fs') //what have you done here
 module.exports.run = async (bot, message, args) => {
   let msgs = JSON.parse(fs.readFileSync('events/msgs.json', 'utf8'));
   let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member
-  if(!message.mentions.members.first() && !message.guild.members.get(args[0])) return message.channel.send({embed:{description:`You have sent ${user.id} messages!`, color:0xff0000}})
+  if(!message.mentions.members.first() && !message.guild.members.get(args[0])) 
+    return message.channel.send({embed:{description: `You have sent ${msgs[user.id].count || '0'} messages`, color:0xff0000}})
   message.channel.send({embed:{description:`${user.user.username} has ${msgs[user.id].count || '0'} messages!`, color:0xff0000}})
 }
 
