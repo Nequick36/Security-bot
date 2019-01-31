c6ec11dca08956fb90411bca7810b9464a3fa718
Glitch (gamehub--2) <none>
2019-01-31 20:01:52 +0000

    🍷☎️ Checkpoint ./commands/await.js:1904606/89

--- commands/await.js
+++ commands/await.js
@@ -4,12 +4,11 @@ module.exports.run = async (bot, message, args) => {
   if(message.author.id !== '435406608479158273') return message.channel.send(`You can't use this command!`)
   let filter = m => m.author.id === message.author.id
   
-  message.reply(`Wait 10 seconds!`)
+  message.reply(`Waiting!`)
   message.channel.awaitMessages(filter, {
-    max: 1,
-    time: 10000
+    max: 1
   }).then(collected => {
-    message.channel.send(collected.content)
+    message.channel.send(collected.first().content)
   })
 }
 
