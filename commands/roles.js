6005e97cff78dfce7e2154a47ee0e3bbc9d00fce
Glitch (gamehub--2) <none>
2019-02-02 19:58:58 +0000

    🐞👲 Checkpoint ./commands/roles.js:1904606/391

--- commands/roles.js
+++ commands/roles.js
@@ -1,17 +1,20 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  let channels = message.guild.channels
-  let mapped = channels.map(channel => {
-    return `\n${channel.name}`
+  let roles = message.guild.roles
+  let sorted = roles.sort(function(a, b) {b.position-a.position})
+  let mapped = sorted.map(role => {
+    return `\n${role.name}   ${role.members.size} members`
   })
+  let splited = mapped.split(',')
+  if(splited.length > 40) return message.channel.send('')
   message.channel.send(`\`\`\`${mapped}\`\`\``)
 }
 
 module.exports.help = {
   name: "roles",
   aliases: [],
-  description: "Shows all channels.",
+  description: "Shows all roles.",
   perm: "",
   role: "",
   group:"Simple"
