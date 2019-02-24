ccfca0bfeb45224d36fb3bfaef1a60fc65d345ff
Glitch (gamehub--2) <none>
2019-02-24 21:55:58 +0000

    🚆🎸 Checkpoint ./index.js:1904606/140

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
+  response.sendFile(__dirname + '/help.html')
 });
 app.listen(process.env.PORT);
 setInterval(() => {
