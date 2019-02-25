96c99753d26a74fed1073f974050fe873192cf19
Glitch (gamehub--2) <none>
2019-02-25 19:11:29 +0000

    🌷👆 Checkpoint ./events/ready.js:1904606/473

--- events/ready.js
+++ events/ready.js
@@ -3,15 +3,16 @@ const discord = require('discord.js')
 exports.run = async (bot) => {
   //rainbow role
   let rainbowrole = bot.guilds.get('358634388461715456').roles.find(r => r.name === 'Rainbow')
-  let colors = [
-    0xff0000,
-    0xffa500,
-    0xffff00,
-    0x008000,
-    0x0000ff,
-    0x4b0082,
-    0xee82ee
-  ]
+  // let colors = [
+  //   0xff0000,
+  //   0xffa500,
+  //   0xffff00,
+  //   0x008000,
+  //   0x0000ff,
+  //   0x4b0082,
+  //   0xee82ee
+  // ]
+  let colors = []
   let num = 0
   setInterval(function() {
     num++
