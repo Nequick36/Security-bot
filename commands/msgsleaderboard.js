d5adbcfe4cfc8195b7c000c022c2384471c856f0
Glitch (gamehub--2) <none>
2019-02-25 20:17:29 +0000

    😳🎙 Checkpoint ./commands/msgsleaderboard.js:1904606/48

--- commands/msgsleaderboard.js
+++ commands/msgsleaderboard.js
@@ -4,9 +4,7 @@ const fs = require('fs') //what have you done here
 module.exports.run = async (bot, message, args) => {
   let msgs = JSON.parse(fs.readFileSync('events/msgs.json', 'utf8'));
   let array = []
-  msgs.forEach(r => {
-    array.push(r)
-  })
+  
   console.log(array)
   array.sort(function(a, b){return a.count - b.count})
 }
