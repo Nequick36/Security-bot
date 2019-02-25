850583bcf33d1b0b6385062fecdfb86bd10d5ebc
Glitch (gamehub--2) <none>
2019-02-25 13:00:07 +0000

    🎂🏎 Checkpoint ./events/message.js:3145912/23 ./commands/ascii.js:3145912/69

--- events/message.js
+++ events/message.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message) => {
   let logChannel = message.guild.channels.find(c => c.name === '🔸》moderation-chat-logs')
   if(message.content.startsWith('!')) logChannel.send({embed:{description: `${message.author.tag} used command ${message.content} in channel ${message.channel.name}.`, color: 0xff0000}})
   if(message.mentions.roles >= 2) {
-     message.delete() //what are you doing suck my w
+     message.delete() //what are you doing 
     message.reply("Don't mention roles more than once please")
   }
   
