61b94a19e40e763b8fda252b60bb2d7af5d2b570
Glitch (gamehubbot) <none>
2019-03-22 19:51:39 +0000

    👡🏯 Checkpoint ./commands/transfer.js:1954975/31 ./watch.json:1954975/2

--- commands/transfer.js
+++ commands/transfer.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   //message.channel.send("This command is still a work in progress!")
   var user = message.mentions.users.first()
     var amount = args[1]
-    if(amount === NaN) return message.reply('Please enter a valid number')
+    if(isNaN(amount)) return message.reply('Please enter a valid number')
  if(message.author.id === user.id) return message.channel.send("Sorry but you can't send money to yourself!")
     if (!user) return message.reply('Reply the user you want to send money to!')
     if (!amount) return message.reply('Specify the amount you want to pay!')
