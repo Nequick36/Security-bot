0c9edc5c32557bab245809ee3198a5c654cd8d16
Glitch (gamehubbot) <none>
2019-03-22 19:40:39 +0000

    👜🏃 Checkpoint ./commands/transfer.js:1954975/384 ./watch.json:1954975/6

--- commands/transfer.js
+++ commands/transfer.js
@@ -4,9 +4,11 @@ module.exports.run = async (bot, message, args) => {
   //message.channel.send("This command is still a work in progress!")
   var user = message.mentions.users.first()
     var amount = args[1]
- 
+    if(amount === NaN) return message.reply('Please enter a valid number')
+ if(message.author.id === user.id) return message.channel.send("Sorry but you can't send money to yourself!")
     if (!user) return message.reply('Reply the user you want to send money to!')
     if (!amount) return message.reply('Specify the amount you want to pay!')
+  
  
     var output = await eco.FetchBalance(message.author.id)
     if (output.balance < amount) return message.reply('You have less coins than the amount you want to transfer!')
