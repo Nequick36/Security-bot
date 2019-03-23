be79897d06145fe77496a3666093cfc1a6f3b179
Glitch (gamehubbot) <none>
2019-03-23 12:03:08 +0000

    🌄🍩 Checkpoint ./commands/announce.js:1954975/120 ./watch.json:1954975/4

--- commands/announce.js
+++ commands/announce.js
@@ -11,7 +11,10 @@ module.exports.run = async (bot, message, args) => {
   if(!channel) return message.channel.send(`Please mention a channel that you want to send announcement!`)
   let announcement = args.slice(1).join(" ")
   if(!announcement) return message.reply("You have not specified a message to announce!")
-  channel.send(announcement)
+  let embed = new Discord.RichEmbed()
+  .setDescription(announcement)
+  .setColor("RED")
+  channel.send(embed)
   message.reply("**Your message has been announced!**")
 }
 
