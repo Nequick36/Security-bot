ee114fafc1271f475f424d8addb30c5e00a9c9f9
Glitch (gamehubbot) <none>
2019-03-24 12:53:16 +0000

    🍵😨 Checkpoint ./commands/ticket.js:1904606/4663

--- commands/ticket.js
+++ commands/ticket.js
@@ -9,34 +9,42 @@ module.exports.run = async (bot, message, args) => {
 !ticket close - Deletes the ticket`)
   }
   if(args[0] === `new`) {
+    let ticketChannel = message.guild.channels.find(r => r.name === `ticket-${message.author.id}`)
+    if(ticketChannel) return message.channel.send(`You can only have one ticket!`)
+    message.guild.createChannel(`ticket-${message.author.id}`, 'text').then(channel => {
+      channel.setTopic(`Owner: ${message.author.tag}`)
+      channel.send(`Hello, ${message.author}. This is your ticket.`)
+      channel.overwritePermissions(message.guild.roles.find(r => r.name === '@everyone'), {
+VIEW_CHANNEL: false,
+SEND_MESSAGES: false,
+READ_MESSAGE_HISTORY: false
+})
+channel.overwritePermissions(message.guild.roles.find(r => r.name === 'Bots'), {
+VIEW_CHANNEL: false,
+SEND_MESSAGES: false,
+READ_MESSAGE_HISTORY: false
+})
+channel.overwritePermissions(message.author, {
+VIEW_CHANNEL: true,
+SEND_MESSAGES: true,
+READ_MESSAGE_HISTORY: true
+})
+channel.overwritePermissions(message.guild.roles.find(r => r.name === 'GameHub Staff'), {
+VIEW_CHANNEL: true,
+SEND_MESSAGES: true,
+READ_MESSAGE_HISTORY: true
+})
+
+    })
     message.guild.channels.forEach(channel => {
       if(channel.name.startsWith('ticket')) {
-        if(channel.topic === `owner:${message.author.id}`) { 
+        if(channel.topic === `owner:${message.author.id}`) {
           return message.channel.send(`You can only have one ticket!`)
         } else {
               message.guild.createChannel(`ticket-${message.author.username}`, 'text').then(channel => {
         channel.setTopic(`owner:${message.author.id}`)
     channel.send(`Hello, ${message.author}. This is your ticket, you can use it for help or applying. Staff Members has access to this channel so don't abuse it. If you want to apply answer the questions(!questions)`)
-          channel.overwritePermissions(message.guild.roles.find(r => r.name === '@everyone'), {
-  VIEW_CHANNEL: false,
-  SEND_MESSAGES: false,
-  READ_MESSAGE_HISTORY: false
-})
-  channel.overwritePermissions(message.guild.roles.find(r => r.name === 'Bots'), {
-  VIEW_CHANNEL: false,
-  SEND_MESSAGES: false,
-  READ_MESSAGE_HISTORY: false
-})
-  channel.overwritePermissions(message.author, {
-  VIEW_CHANNEL: true,
-  SEND_MESSAGES: true,
-  READ_MESSAGE_HISTORY: true
-})
-  channel.overwritePermissions(message.guild.roles.find(r => r.name === 'GameHub Staff'), {
-  VIEW_CHANNEL: true,
-  SEND_MESSAGES: true,
-  READ_MESSAGE_HISTORY: true
-})
+
 })
     ticketChannel = message.guild.channels.find(c => c.name === `ticket-${message.author.username}`)
         }
