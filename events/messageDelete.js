d876c90993d50d1c959fd890e0b10b5e2e8843a9
Glitch (gamehubbot) <none>
2019-03-28 20:34:53 +0000

    🐑💨 Checkpoint ./events/messageDelete.js:1954975/5

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,6 +3,7 @@ const discord = require('discord.js')
 const replaceall = require('replaceall')
 exports.run = async (bot, message) => {
   let logChannel = message.guild.channels.find(c => c.name === '🔸》chat-logs')
+  if
   let embed = new discord.RichEmbed()
   .setTitle(`${message.author.tag}`)
   .setThumbnail(message.author.displayAvatarURL)
