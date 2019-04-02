ea343cb715f7bcecb52c5da3d9d54deb8d81a837
Glitch (gamehubbot) <none>
2019-04-02 15:59:54 +0000

    🐾📹 Checkpoint ./commands/report.js:1954975/592 ./watch.json:1954975/2

--- commands/report.js
+++ commands/report.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-   let reportChannel = message.guild.channel
-  
+   let reportChannel = message.guild.channels.find(channel => channel.name === '⚫》user-reports')
+   let rUser = message.guild.member(message.mentions.users.first())
+   if(!rUser) return message.reply("You have not mentioned a user to report!")
+  let reason = args.slice(1).join(" ")
+  if(!reason) return message.reply("You have not specified a reason")
+  let embed = new Discord.RichEmbed()
+  .setTitle("Report Log")
+  .addField("Reported user", rUser.user.username)
+  .addField("Reported by", message.author.username)
+  .addField("Reported for:", reason)
+  .setColor("GREEN")
+  .setTimestamp()
+  reportChannel.send(embed)
+  message.channel.send()
   
   
   
