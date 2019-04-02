09ad820e5eb43abc48f8d6bcbf07b193857ad29d
Glitch (gamehubbot) <none>
2019-04-02 15:48:35 +0000

    🌷🎎 Checkpoint ./commands/serverinfo.js:1954975/3 ./watch.json:1954975/2 ./commands/report.js:1954975/1613

---
+++ commands/report.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+   let reportChannel = message.guild.channel
+  
+  
+  
+  
+  
+  }
+
+module.exports.help = {
+  name: "report",
+  aliases: [],
+  description: "Reports a user for a specified reason",
+  perm: "",
+  role: "",
+  group:"Admin"
+}
