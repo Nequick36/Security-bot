d1b8bb250994b56b6e42eec6f6bcef59ff25fd3b
Glitch (marble-feels) <none>
2019-08-13 15:03:49 +0000

    🙋🚩 Checkpoint ./commands/ban.js:4316537/3706

--- commands/ban.js
+++ commands/ban.js
@@ -1,20 +1,25 @@
 const Discord = require("discord.js")
-const replaceall = require("replaceall")
 
+exports.run = async (bot, message, args) => {
+let role = message.guild.roles.find(r => r.name === 'Administrator')
+  let bannedUser = message.mentions.members.first();
+       {
+           message.channel.send("🛑**ACCESS DENIED! THIS IS AN ADMIN ONLY COMMAND.🛑**");
+           return;
+       }
 
-module.exports.run = async (bot, message, args) => {
-  
-  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send({embed:{description:` | :x: | Nemas dozvolu za ovu komandu!  | :x: | `, color:0xff0000}})
-    let user = message.mentions.members.first()
-    if(!user) return message.channel.send({embed:{description:`| :x: | Ne mogu da pronadjem tog membera! | :x: |`, color:0xff0000}})
-    let reason = args.filter(arg => arg!==args[0]).join(" ") || 'Razloog nije dat'
-    try {
-        user.ban(`Staff: ${message.author.tag}, Razlog: ${reason}`)
-        message.channel.send({embed:{description:` | :white_check_mark: | ${user} **__je banan zbog__** \`${reason}\``, color:0xff0000}})
-    } catch (e) {
-        if(e) return message.channel.send(e+'\nError: Kontaktirajte ownera!')
-    }
-  
+       if(!bannedUser)
+       {
+           message.channel.send("Sorry, I couldn't find that user");
+           return;
+       }
+      if(message.mentions.members.first().hasPermission('MANAGE_GUILD')) return message.channel.send(`You can't warn a Server Manager!`)
+       let reason = args.slice(1).join(" ")
+       if (!reason)
+       {
+         message.channel.send("You have not specified a reason!")
+         return;
+       }
      
      var banInfo = new Discord.RichEmbed()
          .setTitle("Ban log")
@@ -23,10 +28,10 @@ module.exports.run = async (bot, message, args) => {
          .addField("Reason", reason)
          .setThumbnail(bannedUser.user.avatarURL)
          .setColor(0xFF0000)
-     let banChannel = message.guild.channels.find(channel => channel.name === "logs")
+     let banChannel = message.guild.channels.find(channel => channel.name === "🔸》moderation")
      banChannel.send(banInfo)
      message.delete()
-     bannedUser.send(`You have been banned from GameHub for: ${reason}`).catch(message.channel.send(`** ${bannedUser.user.tag} has been banned!**`))
+     bannedUser.send(`Vi ste banani sa servera ExtremeCommunity, razlog: ${reason}`).catch(message.channel.send(`** ${bannedUser.user.tag} je banovan!**`))
      setTimeout(function(){
   //code
        message.guild.member(bannedUser).ban(7)
@@ -34,14 +39,8 @@ module.exports.run = async (bot, message, args) => {
               .catch(console.error);
 }, 1000);
   }
-
 module.exports.help = {
-  name: "ban",
-  aliases: [],
-  description: "Bans a user for obvious reasons.",
-  perm: "MANAGE_MESSAGES",
-  role: "",
-  group: "Admin"
+name: "ban",
+aliases: ["Ban"]
+
 }
-
-
