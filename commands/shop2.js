f53eca8638c18eb13b66c567b1a0b00228128cbd
Glitch (marble-feels) <none>
2019-08-14 11:55:59 +0000

    ⛴🐪 Checkpoint ./commands/shop.js:4316537/11 ./commands/shop2.js:4316537/2725

--- commands/shop2.js
+++ commands/shop2.js
@@ -7,20 +7,9 @@ module.exports.run = async (bot, message, args) => {
              .setTitle("=================    **__Shop__**    ================")
              .setThumbnail(serverAvatar)
              .addField("**Rankovi**", "**Roles**")
-             .addField("Coal - $250", "Rank")
-             .addField("Iron - $750", "Rank")
-             .addField("Gold - $1000 ", "Rank") 
-             .addField("Redstone - $1500", "Rank")
-             .addField("Lapis - $2500", "Rank")
-             .addField("Diamond - $5000", "Rank")
-             .addField("Emerald - $7500", "Rank")
-             .addField("**Piće**", "**Sokovi**")
-             .addField("Vodka - 5$", "Alkohol")
-             .addField("Kafa - 2$", "Piće")
-             .addField("Piva - 3$", "Alkohol")
+             .addField("Coal - $250 , Iron - $750 , Gold - 1000$ , Redstone - 1500$", "Lapis - 2500$ ,Diamond - 5000$ , Emerald - 7500$ ", "Rank")
+             .addField("**Piće - Sokovi**", "Vodka - 5$ , Piva - 3$ , Kafa - 2$")
              .addField("**Hrana**", "**Food**")
-             .addField("Hljeb - 3$", "Hrana")
-             .addField("Kolač - 4$", "Hrana")
              .addField("=================    **__Shop__**    ================", "**Jos Uskoro**!")
              .setFooter(`en!buy <item-role>`)
              .setColor("GREEN")
@@ -28,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-  name: "shop",
+  name: "shop2",
   aliases: [],
   description:"A shop listing all the different items you can buy.",
   perm: "",
