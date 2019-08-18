1e91b237d72ddc3e91c69c90c3baec7d53b9fdbc
Glitch (najjbolji-bot-official) <none>
2019-08-18 12:03:13 +0000

    🚇🚃 Checkpoint ./commands/report.js:4316537/5941

--- commands/report.js
+++ commands/report.js
@@ -5,41 +5,37 @@ const replaceall = require("replaceall")
 
 module.exports.run = async (bot, message, args) => {
     let warnChannel = message.guild.channels.get("602090584924094470")
-    let modRole = message.guild.roles.find(role => role.name === '@everyone')
-    {
-      
-    }
+
      var user = message.mentions.users.first() 
      if (!user) return message.reply('cannot find user!')
     //if(message.mentions.members.first().hasPermission('MANAGE_GUILD')) return message.channel.send(`You can't warn a Server Manager!`)
-     let report = args.slice(1).join(" ")
-    if (!report)
+     let warning = args.slice(1).join(" ")
+    if (!warning)
       {
         message.channel.send("You have not specified a reason!")
         return;
       }
+  console.log(warning)
       
     message.delete()
-     if(!report[user.id]) report[user.id] = {}
-     if(!report[user.id].report) report[user.id].report = 0
-    if(!report[user.id].reasons) report[user.id].reasons = ''
 
-     var reportChannel = new RichEmbed()
+     var WarningInfo = new RichEmbed()
          .setTitle("Report log")
          .addField("Reported by", message.author.username)
          .addField("Reported User", user.username)
-         .addField('Reports', report[user.id].warns)
-         .addField("Reason", report)
+         .addField("Reason", warning)
          .setColor(0xFF0000)
          .setThumbnail(user.avatarURL)
          .setTimestamp()
-     reportChannel.send(reportChannel)
-}
+     warnChannel.send(WarningInfo)
+
+ }
 module.exports.help = {
     name: "report",
     aliases: ["Report"],
-    description: "Warns a member.",
+    description: "Reports a member.",
   perm: "",
   role: "",
   group: "Simple"
+
 }  
