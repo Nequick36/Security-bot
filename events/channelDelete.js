7a213e178f1ec56029191a99a9eac095458e3cb9
Glitch (najjbolji-bot-official) <none>
2019-08-21 19:37:35 +0000

    👵🌲 Checkpoint ./index.js:4316537/18404 ./events/channelDelete.js:4316537/296 ./event/channelCreate.js:4316537/292 ./events/deleteChannel.js:4316537/292

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const discord = require('discord.js')
 exports.run = async (bot, channel) => {
   let logChannel = bot.guilds.get('602090584924094470').channels.find(c => c.name === 'logs')
-  logChannel.send({embed:{description: `Channel deleted: ${channel.name}`, color: 0xff0000}})
+  logChannel.send({embed:{description: `New channel: ${channel.name} created`, color: 0xff0000}})
 }
