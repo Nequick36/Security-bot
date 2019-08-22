1b617a77c35249c1e4e8a1e17c680d8c04a0731d
Glitch (najjbolji-bot-official) <none>
2019-08-22 20:39:08 +0000

    🎾🐝 Checkpoint ./index.js:4316537/1097

--- index.js
+++ index.js
@@ -159,6 +159,49 @@ xpdelete
 }); 
 
 
+module.exports.run = async (bot, message, args) => {
+
+console.log(`${message.member} ran the "GiveAway" command`);
+
+var item = "";
+var time;
+var winnerCount
+let messageArray = message.content.split(" ");
+//-- f!giveaway 1(winnerCount) 60(seconds) Item Giveaway
+
+for (var i = 3; i < messageArray.length; i++){
+  item += (messageArray[i] + " ");
+}
+winnerCount = Number(messageArray[1]);
+time = Number(messageArray[2]);
+
+
+var giveEmbed = new Discord.RichEmbed();
+giveEmbed.setDescription(item);
+var embedSent = await message.channel.send(giveEmbed);
+embedSent.react("🎉");
+setTimeout(function() {
+
+
+  var peopleReacted = embedSent.reactions.get("🎉").users;
+  var index = Math.floor(Math.random() * peopleReacted.length);
+  var winners = [];
+  var winnerMsg = "";
+  for (var i = 0; i < winners.length; i++){
+    winnerMsg += (winners[i].toString() + " ");
+  }
+  var haveHas = "has";
+  if (winners.length == 1){
+    haveHas = "has";
+  }
+  else{
+    haveHas = "have";
+  }
+  message.channel.send(winnerMsg + " " + haveHas + ` won ${item}`);
+}, time * 1000); //--conversting seconds into miliseconds
+}
+
+
 
 const { Client, Attachment } = require('discord.js');
 
