d8401569cb7a0d5131c4ee376b8a56b5eb60a0fe
Glitch (najjbolji-bot-official) <none>
2019-08-22 21:46:58 +0000

    🍞🔔 Checkpoint ./commands/botinfo.js:4316537/182

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,7 +7,7 @@ exports.run = async (bot,message,args) => {
   .addField("Created By", "This bot created by: ⚡ Veka ⚡ ᴼᴿᴵᴳᴵᴺᴬᴸ#7740")
   .addField("Made With", "[Discord.JS](http://discord.js.org)")
   .addField("Contant Me", "**__⚡ Veka ⚡ ᴼᴿᴵᴳᴵᴺᴬᴸ#7740__**")
-  .addField("So")
+  .addField("Social Media", "[Twitter](https://twitter.com/BlueMalgeran)")
   .setThumbnail(`https://cdn.discordapp.com/attachments/597170622212079646/600627995069382656/ExtremeNetwork.jpg`)
   .setTimestamp() 
 	.setFooter(`${message.guild.name} Bot`, 'https://cdn.discordapp.com/attachments/597170622212079646/600627995069382656/ExtremeNetwork.jpg');
