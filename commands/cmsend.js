68e8a32224337029104f1acbef39efec730c7a40
Glitch (najjbolji-bot-official) <none>
2019-08-28 18:44:48 +0000

    🎟🏐 Checkpoint ./commands/cmsend.js:4316537/114

--- commands/cmsend.js
+++ commands/cmsend.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
-    // Permission Verification (Optional)
-  
+  message.delete()
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send({embed:{description:`| :x: |Nemas dozvolu za ovu komandu! | :x: |`, color:0xff0000}})
-  
+   
 message.channel.send(`
-<> **NOVI MUSIC EVENT**
+< @here > **NOVI MUSIC EVENT** < @here >
 Udjite u voice channel [ **Music Event** ]
 I pustate muziku sa Rythm botom i ko zadnji ostane dobije role Titan i 250$ na nasem Official Botu
 `)
