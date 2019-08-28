11834c77ede4eadeff5791e2b14e85a68bbd4be0
Glitch (najjbolji-bot-official) <none>
2019-08-28 18:09:45 +0000

    ☎️☕️ Checkpoint ./commands/createevent.js:4316537/576

--- commands/createevent.js
+++ commands/createevent.js
@@ -2,23 +2,27 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
   let memberUsername = message.author.username
-  let ticketChannel = message.guild.channels.find(c => c.name === `ticket-${message.author.username}`)
+  let ticketChannel = message.guild.channels.find(c => c.name === `event-${message.author.username}`)
   if(!args[0]) {
     message.channel.send(`Event help:
 en!event new - Makes new event
 en!event close - Deletes the event`)
   }
   if(args[0] === `new`) {
-    let ticketChannel = message.guild.channels.find(r => r.name === `ticket-${message.author.id}`)
+    let ticketChannel = message.guild.channels.find(r => r.name === `event-${message.author.id}`)
     if(ticketChannel) return message.channel.send(`vec imate event u toku !!`)
         message.guild.createChannel(`Member Event`, 'text').then(channel => {
-      channel.setTopic(`Owner: ${message.author.tag}`)
+      channel.setTopic(`Event Created: ${message.author.username}`)
       channel.send(`
-Ćao! @here , ${message.author.tag} je napravio novi **EVENT** pratite upute koje vam napisu!!!
+Ćao! @here , ${message.author.tag} je napravio novi **EVENT** pratite upute koje vam napisem!!!
 **PRAVILA EVENTA**
 Zabranjeno Psovanje (**Warn i Mute 15min**)
 Zabranjeno Vredjanje (**Warn i Mute 15min**)
 Zabranjeno Upadanje u Sred eventa (**Warn i Mute 15min**)
+=======================================================================================================
+**E ovako na nasem serveru** ${message.guild.name} **Dodali smo evente, Vako se to igra**
+**__Vlasnik servera vam kaze da pricate u chatu i ko se prvi javi dobije role Win__**
+**__Onda kad Vlasnik servera kaze da ne pricate u chatu i ko se javi kad je to reko dobije role Fail i ne moze vise ucestvovati__**
 `)
       channel.overwritePermissions(message.guild.roles.find(r => r.name === '@everyone'), {
 VIEW_CHANNEL: true,
