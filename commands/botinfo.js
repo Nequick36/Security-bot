0c9459ed63794cca7c652469b1849c4189bb7c12
Glitch (najjbolji-bot-official) <none>
2019-08-28 14:06:40 +0000

    ☎️🐽 Checkpoint ./commands/botinfo.js:4316537/198

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,7 +7,7 @@ exports.run = async (bot,message,args) => {
   .addField("Created By", "This bot created by: ⚡ Veka ⚡ ᴼᴿᴵᴳᴵᴺᴬᴸ#7740")
   .addField("Made With", "[Discord.PY](https://www.python.org/)")
   .addField("Contant Me", "**__⚡ Veka ⚡ ᴼᴿᴵᴳᴵᴺᴬᴸ#7740__**")
-  .addField("Social Media", "[Twitter](https://twitter.com/Veka14940405) | [YouTube](https://www.youtube.com/channel/UCyahu0PkDygigMlgbShM1nw) | [Instagram](https://www.instagram.com/veka312/?hl=hr)")
+  .addField("Social Media", "| [Twitter](https://twitter.com/Veka14940405) | [YouTube](https://www.youtube.com/channel/UCyahu0PkDygigMlgbShM1nw) | [Instagram](https://www.instagram.com/veka312/?hl=hr) |")
   .setThumbnail(`https://cdn.discordapp.com/attachments/597170622212079646/600627995069382656/ExtremeNetwork.jpg`)
   .setTimestamp() 
 	.setFooter(`${message.guild.name} Bot`, 'https://cdn.discordapp.com/attachments/597170622212079646/600627995069382656/ExtremeNetwork.jpg');
