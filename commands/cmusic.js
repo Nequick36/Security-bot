f5945539aff5f2a13f59232526d87cfe4f37a989
Glitch (najjbolji-bot-official) <none>
2019-08-28 18:20:49 +0000

    🏃🙆 Checkpoint ./commands/createevent.js:4316537/33 ./commands/cmusic.js:4316537/4359

--- commands/cmusic.js
+++ commands/cmusic.js
@@ -2,52 +2,49 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
   let memberUsername = message.author.username
-  let ticketChannel = message.guild.channels.find(c => c.name === `event-${message.author.username}`)
+  let ticketChannel = message.guild.channels.find(c => c.name === `ticket-${message.author.username}`)
   if(!args[0]) {
     message.channel.send(`Event help:
-en!event new - Makes new event
-en!event close - Deletes the event`)
+en!music new - Makes new event
+en!music close - Deletes the event`)
   }
   if(args[0] === `new`) {
     let ticketChannel = message.guild.channels.find(r => r.name === `event-${message.author.id}`)
     if(ticketChannel) return message.channel.send(`vec imate event u toku !!`)
-        message.guild.createChannel(`Member Event`, 'text').then(channel => {
+        message.guild.createChannel(`naruci muziku za event`)
+        message.guild.createChannel(`Music Event`, 'voice').then(channel => {
       channel.setTopic(`Event Created: ${message.author.username}`)
       channel.send(`
-Ćao! @here , ${message.author.tag} je napravio novi **EVENT** pratite upute koje vam napisem!!!
-**PRAVILA EVENTA**
-Zabranjeno Psovanje (**Warn i Mute 15min**)
-Zabranjeno Vredjanje (**Warn i Mute 15min**)
-Zabranjeno Upadanje u Sred eventa (**Warn i Mute 15min**)
-====================================================================================
-**E ovako na nasem serveru** ${message.guild.name} **Dodali smo evente, Vako se to igra**
-**__Vlasnik servera vam kaze da pricate u chatu i ko se prvi javi dobije role Win__**
-**__Onda kad Vlasnik servera kaze da ne pricate u chatu i ko se javi kad je to reko dobije role Fail i ne moze vise ucestvovati__**
+**NOVI MUSIC EVENT**
+Eve
 `)
       channel.overwritePermissions(message.guild.roles.find(r => r.name === '@everyone'), {
-VIEW_CHANNEL: true,
-SEND_MESSAGES: false,
-READ_MESSAGE_HISTORY: true      
+VIEW_CHANNEL: true, 
+SPEAK: false
 })
 channel.overwritePermissions(message.guild.roles.find(r => r.name === 'Fail'), {
 VIEW_CHANNEL: true,
 SEND_MESSAGES: false,
-READ_MESSAGE_HISTORY: true
+READ_MESSAGE_HISTORY: true,
+SPEAK: false
 })
 channel.overwritePermissions(message.author, {
 VIEW_CHANNEL: true,
 SEND_MESSAGES: true,
-READ_MESSAGE_HISTORY: true
+READ_MESSAGE_HISTORY: true,
+SPEAK: true
 })
 channel.overwritePermissions(message.guild.roles.find(r => r.name === 'Staff-Team'), {
 VIEW_CHANNEL: true,
 SEND_MESSAGES: true,
-READ_MESSAGE_HISTORY: true
+READ_MESSAGE_HISTORY: true,
+SPEAK: true
 })
 channel.overwritePermissions(message.guild.roles.find(r => r.name === 'Pristup'), {
 VIEW_CHANNEL: true,
 SEND_MESSAGES: true,
-READ_MESSAGE_HISTORY: true
+READ_MESSAGE_HISTORY: true,
+SPEAK: true
 })
 
     })
@@ -78,8 +75,8 @@ READ_MESSAGE_HISTORY: true
 
 
 module.exports.help = {
-  name: "event",
-  aliases: ["Event"],
+  name: "music",
+  aliases: ["Music"],
   description: "Makes a ticket for help/application",
   perm: "Administrator",
   role: "*",
