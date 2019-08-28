ba93e1cb2c338296e1bce439bae6ab5276726dd9
Glitch (najjbolji-bot-official) <none>
2019-08-28 14:30:40 +0000

    🌌🚨 Checkpoint ./commands/botinfo.js:4316537/128

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -4,7 +4,9 @@ exports.run = async (bot,message,args) => {
      .setColor('#00FF2A')
   
   .setTitle("Bot Info")
+  .setDescription("**Private Bot!**")
   .addField("Created By", "This bot created by: ⚡ Veka ⚡ ᴼᴿᴵᴳᴵᴺᴬᴸ#7740")
+  .addField("Bot Created", "19.5.2019")
   .addField("Made With", "[Discord.PY](https://www.python.org/)")
   .addField("Contant Me", "**__⚡ Veka ⚡ ᴼᴿᴵᴳᴵᴺᴬᴸ#7740__**")
   .addField("Social Media", "| [Twich](https://www.twitch.tv/veka_peka312) | [Twitter](https://twitter.com/Veka14940405) | [YouTube](https://www.youtube.com/channel/UCyahu0PkDygigMlgbShM1nw) | [Instagram](https://www.instagram.com/veka312/?hl=hr) |")
