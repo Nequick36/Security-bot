080b216dd54a94d641ae848e9d7eb8199281e5c0
Glitch (najjbolji-bot-official) <none>
2019-08-29 20:29:19 +0000

    🎩🌿 Checkpoint ./commands/cmusic.js:4316537/186

--- commands/cmusic.js
+++ commands/cmusic.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("🛑 **ACCESS DENIED! THIS IS A MOD/ADMIN ONLY COMMAND. 🛑**")
   let memberUsername = message.author.username
   let ticketChannel = message.guild.channels.find(c => c.name === `music-event`)
   if(!args[0]) {
