b1fb8237877a392a84a5a506a22f6009dd123d9f
Glitch (najjbolji-bot-official) <none>
2019-08-29 10:06:36 +0000

    🚥🐄 Checkpoint ./index.js:4316537/2954

--- index.js
+++ index.js
@@ -158,6 +158,18 @@ xpdelete
 	});
 });
 
+bot.on("ready", () => {
+  const channel = bot.channels.get("601438721346240546");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
+});
+
 
 bot.on("guildMemberAdd", member => {
     let guild = member.guild;
