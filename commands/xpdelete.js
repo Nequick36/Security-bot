b04d8d65df79f35cef94c1605ce4bc500c6faa78
Glitch (najjbolji-bot-official) <none>
2019-09-04 10:17:37 +0000

    👨‍👨‍👦👍 Checkpoint ./commands/xpdelete.js:4316537/17

--- commands/xpdelete.js
+++ commands/xpdelete.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     var user = message.mentions.users.first() || bot.fetchUser(args[0])
     if (!user) return message.reply('Please, Specify a user I have to delete in my database!')
  
-    if (!message.member.hasPermission('ADMINISTRATOR') && !message.member.roles.has(message.guild.roles.find(r => r.name === 'GH Bot Developer').id)) return message.reply('You need to be admin to execute this command!')
+    if (!message.member.hasPermission('ADMINISTRATOR') && !message.member.roles.has(message.guild.roles.find(r => r.name === '*').id)) return message.reply('You need to be admin to execute this command!')
  
     var output = await dl.Delete(user.id)
     if (output.deleted == true) return message.reply('Succesfully deleted the user out of the database!')
