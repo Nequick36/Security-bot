f53ed33af7862a7c4a01ae7ee06efdfe80518faf
Glitch (najjbolji-bot-official) <none>
2019-09-04 07:30:06 +0000

    📙🌧 Checkpoint ./commands/triggered.js:4316537/423

---
+++ commands/triggered.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+module.exports.run = async (bot, message, args) => {
+
+    let profilepic = message.author.avatarURL;
+
+    message.channel.send({
+        file: {
+            attachment: "https://cute-api.tk/v1/generate/triggered?url=" + profilepic,
+            name: "triggered.gif"
+        }
+    });
+
+};
+
+module.exports.help = {
+    name: "triggered",
+    aliases: ["Rriggered"]
+};
