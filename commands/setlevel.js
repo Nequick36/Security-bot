aa8b59e55ef085a4aaef5fcce0553bde1cdfa805
Glitch (najjbolji-bot-official) <none>
2019-09-06 18:37:54 +0000

    🏕🎣 Checkpoint ./staff/lock.js:4316537/910 ./commands/setlevel.js:4316537/122 ./staff/unlock.js:4316537/884 ./vlasnik/nez-staje.js:4316537/7265 ./vlasnik/botavatar.js:4316537/622

--- commands/setlevel.js
+++ commands/setlevel.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const dl = require('discord-leveling')
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`LOL did you really want to cheat?`)
   let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member
   let amount = args[1]
   
