9016a32e91bcf2c5cbb7a7afeaa9aa2ba8e5f110
Glitch (najjbolji-bot-official) <none>
2019-09-18 18:00:43 +0000

    🐸📯 Checkpoint ./commands/shop.js:4316537/1031

--- commands/shop.js
+++ commands/shop.js
@@ -2,6 +2,34 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
   let Embed = new Discord.RichEmbed()
+  message.channel.send(`
+
+
+
+Member+ - 125
+Coal -
+Iron -
+Gold -
+Redstone -
+Lapis -
+Diamond -
+Emerald -           .addField("Member+ - $125", "Rank")
+Vip -        .addField("Coal - $250", "Rank")
+Vip+ -            .addField("Iron - $750", "Rank")
+Premium -            .addField("Gold - $1000 ", "Rank") 
+Ultra -            .addField("Redstone - $1250", "Rank")
+Master - $            .addField("Lapis - $1500", "Rank")
+Supeme - $17500           .addField("Diamond - $3000", "Rank")
+Titan - $200000              .addField("Emerald - $5500", "Rank")
+             .addField("Vip - $8000", "Rank")
+             .addField("Vip+ - $8750", "Rank")
+             .addField("Premium - $10000", "Rank")
+             .addField("Ultra - $12500", "Rank")
+             .addField("Master - $15000", "Rank")
+             .addField("Supreme - $17500", "Rank")
+             .addField("Titan - $20000", "Rank")
+           ("**en!buy <item-name>
+`)
              .addField("Member+ - $125", "Rank")
              .addField("Coal - $250", "Rank")
              .addField("Iron - $750", "Rank")
