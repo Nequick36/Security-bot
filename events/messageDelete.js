9f9a4acfbb4f8f7299d67dd9ffd06ccb60987e19
Glitch (najjbolji-bot-official) <none>
2019-09-18 17:49:38 +0000

    🎃🚎 Checkpoint ./events/messageDelete.js:4316537/7

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,7 +3,7 @@ const discord = require('discord.js')
 const replaceall = require('replaceall')
 exports.run = async (bot, message) => {
   let logChannel = message.guild.channels.find(c => c.name === 'chat-logs')
-  if(message.channel.name === "logs") return;
+  if(message.channel.name === "chat-logs") return;
   let embed = new discord.RichEmbed()
   .setTitle(`${message.author.tag}`)
   .setThumbnail(message.author.displayAvatarURL)
