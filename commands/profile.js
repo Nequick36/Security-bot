d7d56abdc772182064fb91e90a9245ca759d54ce
Glitch (najjbolji-bot-official) <none>
2019-09-22 09:16:11 +0000

    📈👟 Checkpoint ./commands/profile.js:4316537/174

--- commands/profile.js
+++ commands/profile.js
@@ -14,9 +14,10 @@ module.exports.run = async (bot, message, args) => {
         let joined = user.joinedAt
         var profile = new Discord.RichEmbed()
             .addField("Name", user)
-            .addField("Discriminator")
+            .addField("Discriminator", user.discriminator)
             .addField("Joined at", joined) 
             .addField("Coins", userEco.balance)
+            .addField("Level", user.level)
             .addField("Roles", role_name)
             .setColor(0xFF0000)
             .setTitle(`Profile of ${user.user.username}`)
