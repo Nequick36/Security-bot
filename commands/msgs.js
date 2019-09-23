57269deefecd225a072ae502ebdc19310ef23422
Glitch (najjbolji-bot-official) <none>
2019-09-23 17:21:58 +0000

    🙇🍝 Checkpoint ./commands/msgs.js:4316537/8

--- commands/msgs.js
+++ commands/msgs.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
   let msgs = JSON.parse(fs.readFileSync('events/msgs.json', 'utf8'));
   let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member
   if(!message.mentions.members.first() && !message.guild.members.get(args[0])) 
-    return message.channel.send({embed:{description: `Vo ste poslali ${msgs[user.id].Count || '0'} poruka`, color:0xff0000}})
-  message.channel.send({embed:{description:`${user.user.username} je poslao ${msgs[user.id].Count || '0'} poruka!`, color:0xff0000}})
+    return message.channel.send({embed:{description: `Vi ste poslali ${msgs[user.id].count || '0'} poruka`, color:0xff0000}})
+  message.channel.send({embed:{description:`${user.user.username} je poslao ${msgs[user.id].count || '0'} poruka!`, color:0xff0000}})
 }
 
 module.exports.help = {
