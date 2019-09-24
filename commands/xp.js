ec7028cf97480edc5f821c0ee9df2c92441586c7
Glitch (najjbolji-bot-official) <none>
2019-09-24 10:14:01 +0000

    🌎🍼 Checkpoint ./commands/xp.js:4316537/841

--- commands/xp.js
+++ commands/xp.js
@@ -2,19 +2,15 @@ const Discord = require("discord.js");
 const dl = require('discord-leveling')
 
 module.exports.run = async (bot, message, args) => {
-  let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member
-  if(!message.mentions.members.first() && !message.guild.members.get(args[0])) {
-    dl.Fetch(message.author.id).then(l => {
-    message.channel.send(`Vi ste level ${l.level} i imate ${l.xp} xp-a!`)
-  })
-  } else {
-    dl.Fetch(user.id).then(l => {
-    message.channel.send(`${user.user.tag} je level ${l.level} i ima ${l.xp} xp-a!`)
-  })
-  }
+  let Embed = new Discord.RichEmbed()
 
+        .addField("Level", `${l.level}`)
+        .addField("Xp", `${l.xp}`)
+        .setFooter(`${message.author.username}`)
+  
+  
+message.channel.send(Embed)
 }
-
 module.exports.help = {
   name: "xp",
   aliases: ['Xp', 'level'],
