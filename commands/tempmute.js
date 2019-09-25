7cae74a1c5898fb81cfdacbe1bc324d8abba4444
Glitch (najjbolji-bot-official) <none>
2019-09-25 20:13:29 +0000

    🚞🦀 Checkpoint ./commands/tempmute.js:4316537/10409

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -3,9 +3,9 @@ const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.channel.send({embed:{description:`**:x: VI NEMATE DOZVOLU DA KORISITE OVU KOMANDU :x:** `, color:0xff0000}})
-  if(!tomute) return message.reply("**:x: MORATE TAGATI USERA KOJEG ZELITE TEMPMUTATI :x:**");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("**:x: NE MOZETE MUTATI CLANOVE SA DOZVOLOM ADMINISTRATOR :x:**");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("**__:x: VI NEMATE DOZVOLU DA KORISITE OVU KOMANDU :x:__**")
+  if(!tomute) return message.reply("**_:x: MORATE TAGATI MEMBERA KOJEG ZELITE TEMPMUTATI :x:__**");
+  //if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
   let muterole = message.guild.roles.find(`name`, "Extreme | Mute");
 
   //start of create role
@@ -13,43 +13,46 @@ module.exports.run = async (bot, message, args) => {
   if(!muterole){
 
     try{
+
       muterole = await message.guild.createRole({
+
         name: "Extreme | Mute",
-        
-        color: "#BBBBBB",
-        
+
+        color: "#000000",
+
         permissions:[]
 
       })
 
       message.guild.channels.forEach(async (channel, id) => {
+
         await channel.overwritePermissions(muterole, {
-          
-          VIEW_CHANNEL: false,
-          
+
           SEND_MESSAGES: false,
-          
+
           ADD_REACTIONS: false
-          
+
         });
-        
+
       });
-      
+
     }catch(e){
-      
+
       console.log(e.stack);
+
     }
+
   }
-  
+
   //end of create role
-  
+
   let mutetime = args[1]
-  if(!mutetime) return message.reply(":x: Niste stavili vreme! , Upute: en!tempmute <vrjeme> <razlog-muta> :x:");
+  if(!mutetime) return message.reply("Upute: en!tempmute <user> <vrijeme> <razlog> ");
   
   let reason = message.content.split(" ").slice(3).join(" ")
-  if(!reason) return message.reply(":x: Niste stavili razlog, Upute: en!tempmute <vrijeme> <razlog-muta> :x:")
+  if(!reason) return message.reply("Upute: en!tempmute <user> <vrijeme> <razlog> ")
   let embed = new Discord.RichEmbed()
-  .setTitle(`${tomute.user.username} has been muted`)
+  .setTitle(`${tomute.user.username} je tempmutovan`)
   .addField("Duration", mutetime)
   .addField("reason", reason)
   .setThumbnail(tomute.user.avatarURL)
@@ -58,21 +61,22 @@ module.exports.run = async (bot, message, args) => {
 
   await(tomute.addRole(muterole.id));
 
-  message.reply(`<@${tomute.id}> **Je mutovan na:** ${ms(ms(mutetime))}`);
-  message.guild.channels.get("602090584924094470").send(embed)
+  message.reply(`<@${tomute.id}> **__je tempmutovan na:__** __${ms(ms(mutetime))}__`);
+  message.guild.channels.get("619572519074136085").send(embed)
   setTimeout(function(){
     if(!tomute.roles.has(muterole.id)) return;
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> **Je unmutovan/a**`);
-    message.guild.channels.get("602090584924094470").send({embed:{description: `**${tomute.user.username} Je unmutovan/a**`, color:10000}})
+    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.guild.channels.get("619572519074136085").send({embed:{description: `**${tomute.user.username} je unmutovan    **`, color:0xff0000}})
   }, ms(mutetime));
+//end of module
 }
 
 module.exports.help = {
   name: "tempmute",
-  aliases: ["tempMute"],
-  perm: "MANAGE_MESSAGES",
-  role: "EC | Osoblje",
+  aliases: ["Tempmute", "TempMute"],
+  perm: "",
+  role: "EC | Osoblje ",
   group: "Admin"
 
 }
