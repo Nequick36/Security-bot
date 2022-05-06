b33b5a65c2ca1e93ad35baac026c5bcbf8f590bf
Glitch (economy-ec) <none>
2022-05-06 16:07:55 +0000

    ⛺️👐 Checkpoint ./commands/daily.js:4316537/12

--- commands/daily.js
+++ commands/daily.js
@@ -2,19 +2,19 @@ const Discord = require("discord.js");
 const eco = require("discord-economy");
 
 module.exports.run = async (bot, message, args) => {
-  eco.Daily(message.author.id).then(l => {
+  eco.Daily(message.author.id).then(d => {
 //l.updated tells you if the user already claimed his/her daily yes or no.
-               if (l.updated) {
+               if (d.updated) {
 
                  eco.FetchBalance(message.author.id).then(x => {
-                   eco.AddToBalance(250).then(l =>
+                   eco.AddToBalance(250).then(d =>
 message.reply(`**:tada: ___DNEVNA NAGRADA__ :tada:**
-Uzeli ste svoju dnevnu nagradu od 250$ a sada imate ${l.newbalance}
+Uzeli ste svoju dnevnu nagradu od 250$ a sada imate ${d.newbalance}
 `));
                  })
 
                } else {
-                 message.channel.send(`:x: **Vec ste uzeli svoju dnevnu nagradu! da je uzmete ponovo trebate cekati jos:** ${l.timetowait} :x:`)
+                 message.channel.send(`:x: **Vec ste uzeli svoju dnevnu nagradu! da je uzmete ponovo trebate cekati jos:** ${d.timetowait} :x:`)
                }
            })
 }
