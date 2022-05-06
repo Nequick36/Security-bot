388977fdafecdac519d9a4d93d0ebbb3c5d93fa6
Glitch (economy-ec) <none>
2022-05-06 22:31:24 +0000

    🐉🚆 Checkpoint ./commands/help.js:4316537/1590

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-      let embed = new Discord.RichEmbed()
+
+         let embed = new Discord.RichEmbed()
   .setColor("GREEN")
   .setAuthor('CzvVesti | Ekonomija', 'https://i.imgur.com/iSbCziO.jpg' )
   .setFooter("CzvVesti | Admin Team", 'https://i.imgur.com/iSbCziO.jpg' )
@@ -11,11 +12,11 @@ module.exports.run = async (bot, message, args) => {
   .addField("🏪» Shop", "Komanda: .help shop")
   .addField("💳» Balance", "Komanda: .help balance")
   .addField("👮‍♂️» Staff", "Komanda: .help staff")
- if(!args) return message.channel.send(embed);
-
-  
+   return message.channel.send(embed)
+             
   let casino = args[0]
   let kazino = args[0]
+  
         let embed1 = new Discord.RichEmbed()
   .setColor("GREEN")
   .setAuthor('CzvVesti | Ekonomija', 'https://i.imgur.com/iSbCziO.jpg' )
@@ -24,11 +25,9 @@ module.exports.run = async (bot, message, args) => {
   .addField(".casino", "Head-Tails/Glava Pismo prije bacanja novčića birate glavu ili pismu, ako novčić padne na stranu koju sve vi odabrali dobit ćete Novac")
   .addField(".dice", "Dice/Kocka jednostavnim bacanjem kocke dobijate novac ako kocka padne na broj 5 ili 6")
   .addField(".slot", "Fruit Machine/Mašina za voće je mašina za kockanje koja kreira igru na sreću za svoje klijente")
-  if (casino, kazino) return message.channel.send(embed1)
-  
-  
-  
-  message.channel.send(``)
+  if (casino, kazino) {
+    return message.channel.send(embed1)
+  }
   
 }
 module.exports.help = {
