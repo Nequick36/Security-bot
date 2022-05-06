329b15d219ff1a3b56a2af0101de06b2f329fd46
Glitch (economy-ec) <none>
2022-05-06 15:33:41 +0000

    🚃🏬 Checkpoint ./commands/addbalance.js:4316537/1049

--- commands/addbalance.js
+++ commands/addbalance.js
@@ -2,17 +2,27 @@ const Discord = require("discord.js")
 const eco = require("discord-economy")
 
 module.exports.run = async (bot, message, args) => {
-  let amount = message.content.split(" ").slice(2).join(" ");
+  
+          var user = message.mentions.users.first() || message.guild.members.get(args[0])
+
   let embed = new Discord.RichEmbed()
 .setColor("#FF0000")
   .setAuthor('CzvVesti | Pomoć', 'https://i.imgur.com/iSbCziO.jpg' )
   .setFooter("CzvVesti | Admin Team", 'https://i.imgur.com/iSbCziO.jpg' )
- .addField(":x:» Pogreška", "Morate Dodati Iznos Novca")
- .addField("✅» Korišćenje", ".addbalance 100 @User#1991")
-       if (!amount) return message.channel.send(embed)
+ .addField(":x:» Pogreška", "Morate Tagovati Korisnika Kojem Želite Dodat Novac")
+ .addField("✅» Korišćenje", ".addbalance @User#1991 100")
+       if (!user) return message.channel.send(embed)
   
-        var user = message.mentions.users.first() || message.guild.members.get(args[0])
-         if (!user) return message.reply('**__:x: KOJEM MEMBERU ZELITE DODATI COINSA :x:__*')
+    let amount = message.content.split(" ").slice(2).join(" ");
+  let embed2 = new Discord.RichEmbed()
+.setColor("#FF0000")
+  .setAuthor('CzvVesti | Pomoć', 'https://i.imgur.com/iSbCziO.jpg' )
+  .setFooter("CzvVesti | Admin Team", 'https://i.imgur.com/iSbCziO.jpg' )
+ .addField(":x:» Pogreška", "Morate Dodati Iznos Novca")
+ .addField("✅» Korišćenje", ".addbalance @User#1991 100")
+       if (!amount) return message.channel.send(embed2)
+
+        
 
 
         eco.FetchBalance(user.id).then(x => {
