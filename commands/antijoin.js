91ccd65aa504cabf541eb04cc7ff9f3456f0b647
Glitch (economy-ec) <none>
2022-05-07 14:17:50 +0000

    🚤🛰 Checkpoint ./commands/antijoin.js:4316537/1244

--- commands/antijoin.js
+++ commands/antijoin.js
@@ -3,18 +3,12 @@ const eco = require("discord-economy")
 
 module.exports.run = async (bot, message, args) => {
 
-if(args[0] === "on"){
   message.reply("AntiJoin On")
-  bot.on("guildMemberAdd", async (member) => {
-    member.kick({ reason: "Antijoin was enabled"})
-  })
-}
-
-if(args[0] === "off"){
-  message.reply("AntiJoin Off")
-  bot.destroy()
-}
   
+bot.on("guildMemberAdd", (member) => {
+
+})
+
 }
 
 module.exports.help = {
